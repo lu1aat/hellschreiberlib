f57e7f950f27b13bd6020a27de439c9bb03b1708
lu1aat <lu1aat.andres@gmail.com>
2022-08-07 20:16:06 -0300

    Constants: Change int definitions to unsigned.

--- src/Hell.h
+++ src/Hell.h
@@ -8,13 +8,13 @@ class Hell
         void tone();
         void tx(char message[]);
     private:
-        int _pin;
+        unsigned int _pin;
     
-        const int _toneDurationMicrosconds = 8160;  // Tone ("pixel") duration
-        const int _wordSpacingMs = ((_toneDurationMicrosconds / 1000) * 7) * 7;             // Delay spacing between words
-        const int _columns = 7;                     // How many "columns" (pixels wide) have a single character
-        int _hellSymbols;                           // Symbol table, declared in Hell.cpp
-        int _charDelay = (_toneDurationMicrosconds / 1000) * 7;   // Delay between each character of the message in millisencos
+        const unsigned int _toneDurationMicrosconds = 8160;  // Tone ("pixel") duration
+        const unsigned int _wordSpacingMs = ((_toneDurationMicrosconds / 1000) * 7) * 7;             // Delay spacing between words
+        const unsigned int _columns = 7;                     // How many "columns" (pixels wide) have a single character
+        const unsigned int _hellSymbols;                           // Symbol table, declared in Hell.cpp
+        const unsigned int _charDelay = (_toneDurationMicrosconds / 1000) * 7;   // Delay between each character of the message in millisencos
 
         int _charCurrent;
 };
